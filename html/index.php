dc4b980f9b4174a01caf4b6cb231b96c23486f8f
dukhyun <dackhean@gmail.com>
2016-07-04 16:44:46 +0900

    테스트 페이지

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<haed>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</haed>
+<body>
+index.php Test
+<br>
+<span>span css test</span>
+<br>
+<a href="#">alink test</a>
+</body>
+</html>
