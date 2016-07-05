37e76053dc5923816c817a3f69cd723457bcdea6
진두환 <jindoo7409@gmail.com>
2016-07-05 15:02:17 +0900

    회원가입폼완료

--- html/register_form.php
+++ html/register_form.php
@@ -3,17 +3,30 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 <body>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/../include/header.php'; ?>
-<h1>register</h1><br>
-<form action="register_db.php" method="POST">
-	e-mail <input type="text" name="email"><br>
-	nickname <input type="text" name="nick"><br>
-	password <input type="text" name="password"><br>
-	<input type="submit" value="register"><br>
-</form>	
 
+<div class="login center">
+	<h1>00sns register</h1>
+	<form action="register_db.php" method="POST">
+		<ul>
+			<li>
+				<input type="text" name="email" value="E-mail" onfocus="if(this.value == 'E-mail') this.value=''" onblur="if(this.value == '') this.value='E-mail';">
+			</li>
+			<li>
+				<input type="text" name="nick" value="nickname" onfocus="if(this.value == 'nickname') this.value=''" onblur="if(this.value == '') this.value='nickname';">
+			</li>
+			<li>
+				<input type="text" name="password" value="password" onfocus="if(this.value =='password') { this.value=''; this.type='password'; }" onblur="if(this.value =='') { this.value='password'; this.type='text'; }">
+			</li>
+			<li class="floatright">
+				<input type="submit" value="register">
+			</li>
+		</ul>
+	</form>		
+</div>
 <?php // footer ?>
 </body>
 </html>
