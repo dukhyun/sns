a621ff5353eba86f3d4f1565a595db20c475f00f
dukhyun <dackhean@gmail.com>
2016-07-05 14:19:49 +0900

    index.php 로그인 폼 기본 스타일

---
+++ html/dashboard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/../include/header.php' ?>
+
+dashboard.php
+
+<?php // footer ?>
+
+</body>
+</html>
--- html/index.php
+++ html/index.php
@@ -3,16 +3,27 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 <body>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/../include/header.php'; ?>
-<h1>00sns</h1><br>
-<form action="login_db.php" method="POST">
-	e-mail <input type="text" name="email"><br>
-	password <input type="text" name="password"><br>
-	<input type="submit" value="login"><br>
-</form>	
 
+<div class="login center">
+	<h1>00sns</h1>
+	<form action="login_db.php" method="POST">
+		<ul>
+			<li>
+				<input type="text" name="email" value="E-mail" onfocus="if(this.value =='E-mail') this.value=''" onblur="if(this.value =='') this.value='E-mail';">
+			</li>
+			<li>
+				<input type="text" name="password" value="password" onfocus="if(this.value =='password') { this.value=''; this.type='password'; }" onblur="if(this.value =='') { this.value='password'; this.type='text'; }">
+			</li>
+			<li class="floatright">
+				<input type="submit" value="login">
+			</li>
+		</ul>
+	</form>
+</div>
 
 <?php // footer ?>
 
