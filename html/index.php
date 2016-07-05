1a1ab96cac04c2ea085552ab0410ba946f99a296
진두환 <jindoo7409@gmail.com>
2016-07-05 13:52:25 +0900

    index

--- html/index.php
+++ html/index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE HTML>
 <html>
-<haed>
+<head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
-</haed>
+</head>
 <body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/../include/header.php'; ?>
+<h1>00sns</h1><br>
+<form action="login_db.php" method="POST">
+	e-mail <input type="text" name="email"><br>
+	password <input type="text" name="password"><br>
+	<input type="submit" value="login"><br>
+</form>	
 
-<?php include $_SERVER['DOCUMENT_ROOT'].'/../include/header.php' ?>
-
-index.php Test
 
 <?php // footer ?>
 
