84b53672ff10027e02f8539d79b41bcd838db63d
dukhyun <dackhean@gmail.com>
2016-07-08 14:45:26 +0900

    카테고리 수정

--- html/dashboard/category/category_edit.php
+++ html/dashboard/category/category_edit.php
@@ -13,23 +13,29 @@ include_once $root.'/../include/header.php';
 
 <?php // db 불러오기
 $conn = get_connection();
+if (check_login()) {
+	$user_id = get_user_id($conn, $_SESSION['id']);
 ?>
 
 <div class="form_style category_edit center">
 	<h1>카테고리</h1>
 	<?php // 카테고리 리스트 보여주기
-		$result = get_category_list($conn);
+		$result = get_category_list($conn, $user_id);
 		while ($row = mysqli_fetch_assoc($result)) {
-			echo '<ul class="clearboth">';
-			printf('<li class="category_name floatleft">%s</li>', $row['name']);
 			$category_id = $row['id'];
 	?>
-		<li class="floatleft">
-			<form action="update_category.php" method="post">
+	<ul class="category_name clearboth">
+		<form action="update_category.php" method="post">
+			<li class="floatleft">
+	<?php
+		printf('<input type="text" name="name" value="%s">', $row['name']);
+	?>
+			</li>
+			<li class="floatleft">
 				<input type="hidden" name="id" value="<?php echo $category_id; ?>" readonly>
 				<input type="submit" value="수정">
-			</form>
-		</li>
+			</li>
+		</form>
 		<li class="floatleft">
 			<form action="delete_category.php" method="post">
 				<input type="hidden" name="id" value="<?php echo $category_id; ?>" readonly>
@@ -40,7 +46,7 @@ $conn = get_connection();
 	<?php
 		}
 	?>
-	<ul class="clearboth">
+	<ul class="category_name clearboth">
 		<form action="insert_category.php" method="post">
 			<li class="floatleft">
 				<input type="text" name="name">
@@ -58,6 +64,18 @@ $conn = get_connection();
 		</li>
 	</ul>
 </div>
+<script>
+function goBack() {
+    history.back();
+}
+</script>
+<?php
+} else {
+?>
+<script>goBack();</script>
+<?php
+}
+?>
 
 <?php // footer ?>
 
--- html/dashboard/category/update_category.php
+++ html/dashboard/category/update_category.php
@@ -14,10 +14,22 @@ include_once $root.'/../include/header.php';
 <?php
 $conn = get_connection();
 
-if (isset($_POST['id'])) {
+if (isset($_POST['name'], $_POST['id'])) {
 	$id = $_POST['id']; // 카테고리 id
+	$name = $_POST['name']; // 수정할 카테고리 이름
 	
 // 카테고리 수정
+	$update_query = sprintf("UPDATE category SET name = '%s' WHERE id = '%d';", $name, $id);
+	if (mysqli_query($conn, $update_query) === false) {
+		echo mysqli_error($conn);
+	} else {
+		echo 'DB UPDATE<br>';
+		header("Location: category_edit.php");
+	}
+} else {
+	// 카테고리 이름란이 비어있다.
+	header("Location: category_edit.php");
+}
 ?>
 
 <?php // footer ?>
