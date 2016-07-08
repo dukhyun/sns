dd7b9721ca124ca2d1c5c395a1cf8daa899d345f
dukhyun <dackhean@gmail.com>
2016-07-08 12:55:01 +0900

    카데고리 '수정' 버튼 유저 로그인 체크 include 상대경로 수정

--- html/dashboard/post_list.php
+++ html/dashboard/post_list.php
@@ -12,7 +12,7 @@
 	$result = mysqli_query($conn, $select_query);
 	while ($row = mysqli_fetch_assoc($result)) {
 ?>
-	<?php include "post_view.php"; ?>
+	<?php include "./post_view.php"; ?>
 <?php
 	}
 ?>
