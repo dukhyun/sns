ec0034dfc740dfe0f199cad1f9991bf1d5ff0ab3
dukhyun <dackhean@gmail.com>
2016-07-13 16:44:26 +0900

    친구목록 레이아웃 수정

--- html/dashboard/friend/friend_list.php
+++ html/dashboard/friend/friend_list.php
@@ -15,21 +15,23 @@
 	if (check_login()) {
 		$user_id = get_user_id($conn, $_SESSION['id']);
 ?>
-	<h1>친구목록</h1>
+<div>
+	<div class="list center">
+		<h1>친구목록</h1>
 <?php // 친구 리스트 보여주기
 		$select_query = sprintf("SELECT * FROM friend WHERE user_id=%d", $user_id);
 		$result = mysqli_query($conn, $select_query);
 		while ($row = mysqli_fetch_assoc($result)) {
 		?>
 		<form action="delete_friend.php" method="post">
-		<ul>
+		<ul class="clearfix">
 			<li class="floatleft">
 		<?php
 			$friend_id = $row['friend_id'];
 			printf('<a href="dashboard/?user=%d">%s</a>', $friend_id, get_user_nick($conn, $friend_id));
 		?>
 			</li>
-			<li class="floatleft">
+			<li class="floatright">
 				<input type="hidden" name="id" value="<?php echo $friend_id; ?>">
 				<input type="submit" value="삭제">
 			</li>
@@ -39,8 +41,11 @@
 		}
 	}
 ?>
-	<h1>친구검색</h1>
-
+	</div>
+	<div class="list center">
+		<h1>친구검색</h1>
+	</div>
+</div>
 
 <?php // footer ?>
 
